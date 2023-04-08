1257490ba7d02e2ace0dc5bade11e4d2c6dd550a
hho <khanipovm@gmail.com>
2023-04-08 04:19:51 +0300

    user model

--- backend/db/models/user.py
+++ backend/db/models/user.py
@@ -1,10 +1,16 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+import sqlalchemy as db
 from backend.db.db import Base
 
-class User(Base):
-    __tablename__ = "users"
+class BaseUser(Base):
+    __tablename__ = "base_users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    email = db.Column(db.String, unique=True, index=True)
+    name = db.Column(db.String)
+    surname = db.Column(db.String)
+    middlename = db.Column(db.String)
+    created_at = db.Column(db.DateTime, nullable=False, server_default=db.sql.func.now())
+    updated_at = db.Column(db.DateTime,  nullable=False, server_default=db.sql.func.now(), onupdate=db.sql.func.now())
+    hashed_password = db.Column(db.String)
+    is_active = db.Column(db.Boolean, default=True)
+
--- backend/db/schemas/user.py
+++ backend/db/schemas/user.py
@@ -1,18 +1,24 @@
+from datetime import datetime
 from typing import Union
 from pydantic import BaseModel
 
 
-class UserBase(BaseModel):
+class BaseUserBase(BaseModel):
     email: str
+    name: str
+    surname: str
+    middlename: str
 
 
-class UserCreate(UserBase):
+class BaseUserCreate(BaseUserBase):
     password: str
 
 
-class User(UserBase):
+class BaseUser(BaseUserBase):
     id: int
     is_active: bool = True
+    created_at: datetime
+    updated_at: datetime
     
     class Config:
         orm_mode = True
