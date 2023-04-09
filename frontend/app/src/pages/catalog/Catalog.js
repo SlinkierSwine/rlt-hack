929bb47bcd257893a929a9e513b194ca111035f4
SashkaAlkashka <113834536+SashkaAlkashka@users.noreply.github.com>
2023-04-09 14:04:58 +0300

    add catalog page

--- frontend/app/src/pages/catalog/Catalog.js
+++ frontend/app/src/pages/catalog/Catalog.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { Fragment } from "react";
 import './Catalog.scss'
+import {Header} from '../../components/header/Header'
+import star from '../../image/star.svg'
+import {NavLink} from 'react-router-dom'
 
 export const Catalog = () => {
     return(
-        <h1>Catalog</h1>
+        <Fragment>
+            <Header/>
+            <div className="catalogContent">
+                <h1>Ежедневные квесты</h1>
+            <div className="lessons">
+                    <NavLink>
+                        <div className="lesson">
+                            <div className="backLesson"></div>
+                            <div className="lessonContent">
+                                <div className="hLesson">Квесты</div>
+                                <div className="chapter">Правильный выбор</div>
+                            </div>
+                        </div>
+                    </NavLink>
+                    <NavLink>
+                        <div className="lesson">
+                            <div className="backLesson"></div>
+                            <div className="lessonContent">
+                                <div className="hLesson">Квесты</div>
+                                <div className="chapter">Правильный выбор</div>
+                            </div>
+                        </div>
+                    </NavLink>
+                </div>  
+            </div>
+        </Fragment>
     )
 }
