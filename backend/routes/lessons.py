2bdde05d7feb495d29eddf575c7a4ef2bffca8d1
Mihail-v2 <mihail.nuridinov555@gmail.com>
2023-04-09 12:24:15 +0300

    update function get and post

--- backend/routes/lessons.py
+++ backend/routes/lessons.py
@@ -1,17 +1,19 @@
-from fastapi import APIRouter, Depends
 from pydantic import BaseModel
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from backend.db.schemas import course as schema
+from backend.db.models import course as model
+from backend.db.crud import course as crud
 from backend.db.db import get_db
+import json
 
 
 router = APIRouter(prefix="/lesson/")
 
 @router.get("/")
-def lesson_page(db: Session = Depends(get_db)):
-    response = {'message': 'return JSON data. Lesson'}
-    #TODO: Collect all data from databases
-    #response = json.dumps(response)
-    #response = json.loads(response)
+def lesson_page(course: schema.CourseCreate, db: Session = Depends(get_db)):
+    db_course = crud.get_lesson(db, id=course.id)
+    response = {'message': db_course}
     return response
 
 
@@ -23,12 +25,12 @@ class TaskRequest(BaseModel):
 
 
 @router.post("/")
-def change_task(db: Session = Depends(get_db), finished_button: BaseModel = False) -> dict:
-    response = {'message': 'return JSON data. Task'}
+def change_task(course: schema.CourseCreate, db: Session = Depends(get_db), finished_button: BaseModel = False) -> dict:
+    db_course = crud.get_lesson(db, id=course.id)
+    response = {'message': db_course}
     #TODO: Update data in database
     if not finished_button:
         #TODO: Collect all data from the database
         #response = json.dumps(response)
         #response = json.loads(response)
         return response
-    
